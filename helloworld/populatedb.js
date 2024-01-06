b397b8bfa08acc63c6c447e543cb180f95bb73a3
Negrea-Andrei <negrea97@yahoo.com>
2024-01-06 22:50:43 +0200

    eliminate genres

--- helloworld/populatedb.js
+++ helloworld/populatedb.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 console.log(
-    'This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: node populatedb "mongodb+srv://cooluser:coolpassword@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority"'
+    'This script populates some test books, authors, and bookinstances to your database. Specified database as argument - e.g.: node populatedb "mongodb+srv://cooluser:coolpassword@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority"'
   );
   
   // Get arguments passed on command line
@@ -11,7 +11,6 @@ console.log(
   const Author = require("./models/author");
   const BookInstance = require("./models/bookinstance");
   
-  const genres = [];
   const authors = [];
   const books = [];
   const bookinstances = [];
@@ -79,15 +78,7 @@ console.log(
     console.log(`Added bookinstance: ${imprint}`);
   }
   
-  async function createGenres() {
-    console.log("Adding genres");
-    await Promise.all([
-      genreCreate(0, "Fantasy"),
-      genreCreate(1, "Science Fiction"),
-      genreCreate(2, "French Poetry"),
-    ]);
-  }
-  
+    
   async function createAuthors() {
     console.log("Adding authors");
     await Promise.all([
@@ -106,43 +97,37 @@ console.log(
         "The Name of the Wind (The Kingkiller Chronicle, #1)",
         "I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep.",
         "9781473211896",
-        authors[0],
-        [genres[0]]
+        authors[0]
       ),
       bookCreate(1,
         "The Wise Man's Fear (The Kingkiller Chronicle, #2)",
         "Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.",
         "9788401352836",
-        authors[0],
-        [genres[0]]
+        authors[0]
       ),
       bookCreate(2,
         "The Slow Regard of Silent Things (Kingkiller Chronicle)",
         "Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.",
         "9780756411336",
-        authors[0],
-        [genres[0]]
+        authors[0]
       ),
       bookCreate(3,
         "Apes and Angels",
         "Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...",
         "9780765379528",
-        authors[1],
-        [genres[1]]
+        authors[1]
       ),
       bookCreate(4,
         "Death Wave",
         "In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...",
         "9780765379504",
-        authors[1],
-        [genres[1]]
+        authors[1]
       ),
       bookCreate(5,
         "Test Book 1",
         "Summary of test book 1",
         "ISBN111111",
-        authors[4],
-        [genres[0], genres[1]]
+        authors[4]
       ),
       bookCreate(6,
         "Test Book 2",
